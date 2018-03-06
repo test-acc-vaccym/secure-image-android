81cfbd7dc82eeed86f76903d027be7b81a4d3316
“ShellyXueHan” <shelly@freshworks.io>
2018-03-05 17:16:47 -0800

    switched to sample test case

---
+++ bdd/geb-mobile/geb-mobile-test-spock/src/test/groovy/geb/mobile/android/activities/WikisearchActivity.groovy
@@ -0,0 +1,24 @@
+package geb.mobile.android.activities
+
+import geb.mobile.android.AndroidBaseActivity
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class WikisearchActivity extends AndroidBaseActivity {
+
+    static content = {
+        searchButton {find("~Search Wikipedia")}
+        searchText {$("#search_src_text")}
+        resultList {$("android.widget.TextView")}
+        targetResult {$("//android.widget.TextView[@text='HyperCard']")}
+
+        swipeAction {
+            if(!targetResult) {
+                swipeDown()
+            } else {
+                back()
+                return true
+            }
+        }
+    }
+}
--- bdd/geb-mobile/geb-mobile-test-spock/src/test/groovy/geb/mobile/android/specification/GebMobileAutomationTestWithPagesSpec.groovy
+++ bdd/geb-mobile/geb-mobile-test-spock/src/test/groovy/geb/mobile/android/specification/GebMobileAutomationTestWithPagesSpec.groovy
@@ -11,6 +11,7 @@ import geb.mobile.android.activities.ChromSigninActivity
 import geb.mobile.android.activities.AccessPermissionActivity
 import geb.mobile.android.activities.RedirectActivity
 import geb.mobile.android.activities.SettingsActivity
+import geb.mobile.android.activities.WikisearchActivity
 
 
 import spock.lang.Stepwise
@@ -29,153 +30,130 @@ import spock.lang.Stepwise
 @Stepwise
 class GebMobileAutomationTestWithPagesSpec extends GebMobileBaseSpec {
 
-    def "0. Set screen lock PIN in settings"() {
-        given: "I go to settings"
-        at SettingsActivity
 
-        when:
-        lockTab.click()
-        sleep(500)
+    def "open test-app and enter search string "() {
+        given: "I land on Home screen"
+        at WikisearchActivity
 
-        and:
-        lockType.click()
-        sleep(500)
-        pinOptions.click()
-        sleep(500)
-        pinField = "123456"
-        sleep(500)
+        when: "I click on the search button"
+        searchButton.click()
 
-        and:
-        nextButton.click()
+        and: "I type in search string and scroll down"
+        searchText = "BrowserStack"
+        swipeAction
 
-        and:
-        pinField = "123456"
-        nextButton.click()
-
-        then:
-        switchToAPP
-
+        then: "I should see results"
+        assert resultList.size() > 0
     }
 
-//    def "1. First time login to the app"() {
-//        given: "I open the app"
-//        at FirstTimeLoginActivity
-//        switchApp
-//        sleep(1000)
-//
-//        when:
-//        at SettingsActivity
-//        lockTab.click()
-//        sleep(500)
-//
-//        and:
-//        lockType.click()
-//        sleep(500)
-//        pinOptions.click()
-//        sleep(500)
-//        pinField = "123456"
-//        sleep(500)
-//
-//        and:
-//        nextButton.click()
-//
-//        and:
-//        pinField = "123456"
-//        nextButton.click()
-//
-//        and:
-//        switchApp2
-//        sleep(500)
-//
-//
-//
-//        then: "I should see the homepage to create album"
-//        at FirstTimeLoginActivity
-//        authenticateButton.click()
-//        sleep(1000)
-//
-//    }
+    // def "0. Set screen lock PIN in settings"() {
+    //     given: "I go to settings"
+    //     at SettingsActivity
 
-    def "1. First time login to the app"() {
-        given: "I open the app"
-        at FirstTimeLoginActivity
+    //     when:
+    //     lockTab.click()
+    //     sleep(500)
 
-        when: "I click on the auth button"
-        authenticateButton.click()
-        sleep(500)
+    //     and:
+    //     lockType.click()
+    //     sleep(500)
+    //     pinOptions.click()
+    //     sleep(500)
+    //     pinField = "123456"
+    //     sleep(500)
 
-        and: "I type in password to unlock and continue"
-//        this is the device password
-        passwordField = "123456"
-        sleep(500)
-        nextButton.click()
-        sleep(500)
+    //     and:
+    //     nextButton.click()
 
-        and: "I see the camera access request message and allow"
-        at AccessPermissionActivity
-        allowAccessButton.click()
-        sleep(1000)
+    //     and:
+    //     pinField = "123456"
+    //     nextButton.click()
 
-        then: "I should see the homepage to create album"
-        at HomeScreenActivity
-        createAlbumButton.click()
-    }
+    //     then:
+    //     switchToAPP
 
-    def "2. Create empty albums"() {
-        given: "I am at the create album page"
-        at AlbumCreationActivity
+    // }
 
-        when: "I click on the back button"
-        backButton.click()
+//     def "1. First time login to the app"() {
+//         given: "I open the app"
+//         at FirstTimeLoginActivity
 
-        and:
-        at HomeScreenActivity
-        createAlbumButton.click()
+//         when: "I click on the auth button"
+//         authenticateButton.click()
+//         sleep(500)
 
-        and:
-        at AlbumCreationActivity
-        backButton.click()
-        sleep(1000)
+//         and: "I type in password to unlock and continue"
+// //        this is the device password
+//         passwordField = "123456"
+//         sleep(500)
+//         nextButton.click()
+//         sleep(500)
 
-        then: "I should see the homepage to create album"
-        at HomeScreenActivity
-        assert albums.size() >= 1
-        createAlbumButton.click()
-    }
+//         and: "I see the camera access request message and allow"
+//         at AccessPermissionActivity
+//         allowAccessButton.click()
+//         sleep(1000)
+
+//         then: "I should see the homepage to create album"
+//         at HomeScreenActivity
+//         createAlbumButton.click()
+//     }
+
+//     def "2. Create empty albums"() {
+//         given: "I am at the create album page"
+//         at AlbumCreationActivity
+
+//         when: "I click on the back button"
+//         backButton.click()
+
+//         and:
+//         at HomeScreenActivity
+//         createAlbumButton.click()
+
+//         and:
+//         at AlbumCreationActivity
+//         backButton.click()
+//         sleep(1000)
+
+//         then: "I should see the homepage to create album"
+//         at HomeScreenActivity
+//         assert albums.size() >= 1
+//         createAlbumButton.click()
+//     }
 
 
-    def "3. Create an album"() {
-        given: "I am at the create album page"
-        at AlbumCreationActivity
+//     def "3. Create an album"() {
+//         given: "I am at the create album page"
+//         at AlbumCreationActivity
 
-        when: "I click on the add image button"
-        addImageButton.click()
+//         when: "I click on the add image button"
+//         addImageButton.click()
 
-        and:
-        at AccessPermissionActivity
-        allowAccessButton.click()
-        sleep(3000)
+//         and:
+//         at AccessPermissionActivity
+//         allowAccessButton.click()
+//         sleep(3000)
 
-        and:
-        at TakeImageActivity
-        shutterButton.click()
-        sleep(500)
-        shutterButton.click()
-        sleep(500)
-        backButton.click()
-        sleep(500)
+//         and:
+//         at TakeImageActivity
+//         shutterButton.click()
+//         sleep(500)
+//         shutterButton.click()
+//         sleep(500)
+//         backButton.click()
+//         sleep(500)
 
 
-        and:
-        at AlbumCreationActivity
-        uploadButton.click()
-        sleep(1000)
+//         and:
+//         at AlbumCreationActivity
+//         uploadButton.click()
+//         sleep(1000)
 
-        then: "I should see the page to redirect to webview"
-        at RedirectActivity
-        loginButton.click()
+//         then: "I should see the page to redirect to webview"
+//         at RedirectActivity
+//         loginButton.click()
 
-    }
+//     }
 
 
 
